d0e380571d54ce842143093790acab5f7e5e4a5b
Rise Erpelding <rise@heysparkbox.com>
2023-02-21 15:22:13 -1000

    feat: get tile colors from state

--- components/ColorGrid.jsx
+++ components/ColorGrid.jsx
@@ -1,28 +1,43 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-export const ColorGrid = () => {
+export const ColorGrid = ({ colors }) => {
+  const initStylesFromProps = {
+    topLeft: {backgroundColor: 'red'},
+    topRight: {backgroundColor: 'blue'},
+    bottomLeft: {backgroundColor: 'yellow'},
+    bottomRight: {backgroundColor: 'green'},
+  };
+  const [colorStyles, setColorStyles] = useState(initStylesFromProps);
+  const setColorStyleVariables = (colorsObj) => {
+    const propColorStyles = {};
+    for (const [key, value] of Object.entries(colorsObj)) {
+      propColorStyles[key] = {backgroundColor: value};
+    }
+    setColorStyles(propColorStyles);
+  };
+
+  useEffect(() => {
+    setColorStyleVariables(colors);
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.row}>
-        <View style={styles.topLeft} />
-        <View style={styles.topRight} />
+        <View style={[styles.topLeft, colorStyles.topLeft]} />
+        <View style={[styles.topRight, colorStyles.topRight]} />
       </View>
       <View style={styles.row}>
-        <View style={styles.bottomLeft} />
-        <View style={styles.bottomRight} />
+        <View style={[styles.bottomLeft, colorStyles.bottomLeft]} />
+        <View style={[styles.bottomRight, colorStyles.bottomRight]} />
       </View>
     </View>
   );
 }
-
 const styleVariables = {
   containerSize: 160,
   boxSize: 80,
-  colorOne: 'blue',
-  colorTwo: 'green',
-  colorThree: 'red',
-  colorFour: 'yellow',
   borderColor: 'gray',
 };
 
@@ -42,24 +57,20 @@ const styles = StyleSheet.create({
     height: styleVariables.boxSize,
     width: styleVariables.boxSize,
     borderColor: styleVariables.borderColor,
-    backgroundColor: styleVariables.colorOne,
   },
   topRight: {
     height: styleVariables.boxSize,
     width: styleVariables.boxSize,
     borderColor: styleVariables.borderColor,
-    backgroundColor: styleVariables.colorTwo,
   },
   bottomLeft: {
     height: styleVariables.boxSize,
     width: styleVariables.boxSize,
     borderColor: styleVariables.borderColor,
-    backgroundColor: styleVariables.colorThree,
   },
   bottomRight: {
     height: styleVariables.boxSize,
     width: styleVariables.boxSize,
     borderColor: styleVariables.borderColor,
-    backgroundColor: styleVariables.colorFour,
   },
 });
