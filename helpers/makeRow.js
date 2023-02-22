771212ef4887c4bfbd80fa78d9a8ba9db9b1339e
Rise Erpelding <rise@heysparkbox.com>
2023-02-21 14:26:14 -1000

    feat: add helpers

---
+++ helpers/makeRow.js
@@ -0,0 +1,18 @@
+import { convertHexToRGB, convertRGBToHex } from "./colorHelpers";
+import { makeStops } from "./makeStops";
+
+export const makeRow = (length, startColor, endColor) => {
+  const row = new Array(length);
+  const startColorRGB = convertHexToRGB(startColor);
+  const endColorRGB = convertHexToRGB(endColor);
+
+  const rStops = makeStops(length, startColorRGB.r, endColorRGB.r);
+  const gStops = makeStops(length, startColorRGB.g, endColorRGB.g);
+  const bStops = makeStops(length, startColorRGB.b, endColorRGB.b);
+
+  for (let i = 0; i < row.length; i += 1) {
+    row[i] = convertRGBToHex(rStops[i], gStops[i], bStops[i]);
+  }
+  // return row.map((value, i) => convertRGBToHex(rStops[i], gStops[i], bStops[i]));
+  return row;
+};
