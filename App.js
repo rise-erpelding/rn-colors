1c65347246e152d1c4ae5b1b6abceb862d918318
Rise Erpelding <rise@heysparkbox.com>
2023-03-21 20:10:26 -1000

    feat: set up dynamic grid

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Landing } from './src/components/Landing'
+import { Landing } from './src/screens/Landing'
 
 export default function App() {
   return (
