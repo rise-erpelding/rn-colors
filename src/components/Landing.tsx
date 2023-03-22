bf9ea358886f3a7302a74003818e436d423e9bd7
Rise Erpelding <rise@heysparkbox.com>
2023-03-21 19:20:10 -1000

    feat: add shuffle colors button

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -14,6 +14,10 @@ export const Landing = () => {
     console.log('making grid')
   }
 
+  const shuffleColors = () => {
+    console.log('shuffle colors')
+  }
+
   return (
     <View style={styles.container}>
       <Text>Tiles!</Text>
@@ -21,6 +25,7 @@ export const Landing = () => {
         colors={colors}
       />
       {/* difficulty controls here */}
+      <Button title="Shuffle Colors" onPress={shuffleColors}></Button>
       <Button title="Go" onPress={makeGrid}></Button>
     </View>
   );
