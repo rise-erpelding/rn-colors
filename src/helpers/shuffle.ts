f91c5ead3008ef1eb2a9e353b49dbc45110cafc2
Rise Erpelding <rise@heysparkbox.com>
2023-03-24 12:15:45 -1000

    feat: build shuffle colors feature

---
+++ src/helpers/shuffle.ts
@@ -0,0 +1,7 @@
+export function shuffle(arr: any[]) { // Fisher-Yates shuffle
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
